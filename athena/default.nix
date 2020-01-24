7fb899098111f1054a0f1aed50d1dd9c3e2fa8a2
Anders Kaseorg <andersk@mit.edu>
2020-01-24 18:29:22 -0500

    Add mit-cmu license
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- athena/default.nix
+++ athena/default.nix
@@ -4,4 +4,5 @@ lib.makeScope newScope (self:
   with self; {
     aclocal = callPackage ./aclocal { };
     hesiod = callPackage ./hesiod { };
+    licenses = import ./licenses.nix;
   })
---
+++ athena/licenses.nix
@@ -0,0 +1,8 @@
+{
+  mit-cmu = {
+    fullName = "CMU License";
+    shortName = "mit-cmu";
+    spdxId = "MIT-CMU";
+    url = "https://spdx.org/licenses/MIT-CMU.html";
+  };
+}
