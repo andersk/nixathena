ae1dc212bba1ba4b5b5681f9fd51361f3e09b14d
Anders Kaseorg <andersk@mit.edu>
2020-01-24 18:29:43 -0500

    python2Packages.PyHesiod: init at 0.2.12
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- athena/python-modules/default.nix
+++ athena/python-modules/default.nix
@@ -3,4 +3,5 @@
 {
   discuss = callPackage ./discuss { };
   PyAFS = callPackage ./afs { };
+  PyHesiod = callPackage ./hesiod { };
 }
---
+++ athena/python-modules/hesiod/default.nix
@@ -0,0 +1,24 @@
+{ buildPythonPackage, fetchFromGitHub, hesiod, lib, pyrex }:
+
+buildPythonPackage rec {
+  pname = "PyHesiod";
+  version = "0.2.12";
+  src = fetchFromGitHub {
+    owner = "mit-athena";
+    repo = "python-hesiod";
+    rev = version;
+    sha256 = "0r1hh1nnw1k7z03i29zxpd7fmcx5fga6z5yl7950ik8nx77dg4zf";
+  };
+  buildInputs = [ hesiod ];
+  propagatedBuildInputs = [ pyrex ];
+
+  meta = {
+    description = "Python bindings for Hesiod";
+    longDescription = ''
+      PyHesiod provides a set of Python bindings for Hesiod, the Project
+      Athena service name resolution protocol used at MIT and elsewhere.
+    '';
+    homepage = "https://github.com/mit-athena/python-hesiod";
+    license = lib.licenses.mit;
+  };
+}
