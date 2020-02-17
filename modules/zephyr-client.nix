e2686ed61d83ed9cd48482b920bcf9f50db0beee
Anders Kaseorg <andersk@mit.edu>
2020-02-16 19:20:02 -0800

    zephyrClient module: Add openFirewall option
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- modules/zephyr-client.nix
+++ modules/zephyr-client.nix
@@ -9,6 +9,13 @@ in {
         type = lib.types.bool;
         description = "Whether to enable the Zephyr host manager.";
       };
+
+      openFirewall = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description =
+          "Whether to open ports in the firewall for the Zephyr host manager.";
+      };
     };
   };
 
@@ -17,6 +24,8 @@ in {
 
     networking.athena.hesiod.enable = lib.mkDefault true;
     networking.athena.kerberos.enable = lib.mkDefault true;
+    networking.firewall =
+      lib.mkIf cfg.openFirewall { allowedUDPPorts = [ 2104 ]; };
 
     systemd.services.athena-zhm = {
       description = "Zephyr host manager";
